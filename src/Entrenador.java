ed6daec151db5dd5f627160baed2f760e3034ddb
mac20_lc3 <cecyparamo94@gmail.com>
2022-05-03 11:50:46 -0500

    tambien arregalos la calse entrenador

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Entrenador extends Personaje {
     //region -String
     //torneos ganados -int
@@ -9,4 +11,58 @@ public class Entrenador extends Personaje {
       //si su HP es superior a 15
        //escoger 2 fuertes y 1 debil aleatorio
     //sino tiene fuertes poner 3 aleatorios
+    private String region;
+    private Pokemon pokemonMacota;
+    private int torneosGanados;
+    private ArrayList <Pokemon>pokedex;
+    private  ArrayList <Objeto> moschila;
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public Pokemon getPokemonMacota() {
+        return pokemonMacota;
+    }
+
+    public void setPokemonMacota(Pokemon pokemonMacota) {
+        this.pokemonMacota = pokemonMacota;
+    }
+
+    public int getTorneosGanados() {
+        return torneosGanados;
+    }
+
+    public void setTorneosGanados(int torneosGanados) {
+        this.torneosGanados = torneosGanados;
+    }
+
+    public ArrayList<Pokemon> getPokedex() {
+        return pokedex;
+    }
+
+    public void setPokedex(ArrayList<Pokemon> pokedex) {
+        this.pokedex = pokedex;
+    }
+
+    public ArrayList<Objeto> getMoschila() {
+        return moschila;
+    }
+
+    public void setMoschila(ArrayList<Objeto> moschila) {
+        this.moschila = moschila;
+    }
+
+    public Entrenador(String nombre, int nivel, char genero, String region, Pokemon pokemonMacota, int torneosGanados, ArrayList<Pokemon> pokedex, ArrayList<Objeto> moschila) {
+        super(nombre, nivel, genero);
+        this.region = region;
+        this.pokemonMacota = pokemonMacota;
+        this.torneosGanados = torneosGanados;
+        this.pokedex = pokedex;
+        this.moschila = moschila;
+    }
 }
