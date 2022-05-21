265933b2cdc85a7b49d07626fdf6e672c5cb95d5
Cecyyy <cecyparamo94@gmail.com>
2022-05-21 13:39:04 -0500

    continuacion ahora clase baya

--- src/Baya.java
+++ src/Baya.java
@@ -33,5 +33,30 @@
             this.efecto = efecto;
             this.duracion = duracion;
         }
+
+        public boolean usar(Pokemon pokemon){
+            if (this.efecto.equals("curacion")){
+                pokemon.setHp(pokemon.getHp()+20);
+                return true;
+            }else if (this.efecto.equals(("furza"))){
+                pokemon.getHabilidad().setataqueBase(pokemon.getHabilidad().getataqueBase()+20);
+                return true;
+
+            }else if (this.efecto.equals("velocidad")){
+                pokemon.setVelocidad(pokemon.getVelocidad()+20);
+                return true;
+            }else{
+                return false;
+            }
+        }
+        //rear toString en todaslas clases
+
+        @Override
+        public String toString() {
+            return "Baya{" +
+                    "efecto='" + efecto + '\'' +
+                    ", duracion=" + duracion +
+                    '}';
+        }
     }
 
