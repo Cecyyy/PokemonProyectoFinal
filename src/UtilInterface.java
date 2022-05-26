24071458f2297e744d9cabba4fc7f673642afb7c
mac20_lc3 <cecyparamo94@gmail.com>
2022-05-26 11:47:35 -0500

    seguimos con pelear

--- src/UtilInterface.java
+++ src/UtilInterface.java
@@ -9,8 +9,8 @@ public interface UtilInterface {
     //mostrar menu----> void con lista de acciones a realizar
     void monstrarMenu();
 
-    //crear pokemons ---> aleatorio
-    Pokemon crearPokemonsAleatorio();
+    //crear pokemones ---> aleatorio
+    Pokemon crearPokemonesAleatorio();
 
     //generico sacar aleatorio = sacar un elemento generico aleatorio
     //N de namjoon <3
@@ -22,4 +22,9 @@ public interface UtilInterface {
     // string [] nombres pokemons = devolver un arreglo de string
     String[] nombresaleatorios (String tipo);
 
+    // main com implements y nombre de la interface
+    //se crean de formaa estatica los elemntos a implementar
+    //un main diferente al main original
+    //ToDo
+    //el main con la inferface, void mostrar menu y tiempo jugado
 }
