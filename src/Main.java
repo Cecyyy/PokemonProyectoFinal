1b1beb048a0e22ee647cb344758f6c8f413e9275
Cecyyy <cecyparamo94@gmail.com>
2022-05-30 17:54:47 -0500

    seguimos con las habilidades, hice seis

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 import java.util.ArrayList;
 
 public class Main implements UtilInterface {
-    static ArrayList <Habilidad> habilidads =new ArrayList<>();
+    static ArrayList <Habilidad> habilidades =new ArrayList<>();
     static Habilidad habilidadUno =new Habilidad("CONFUSION","ataque psiquico ", +82);
     static Habilidad habilidadDos = new Habilidad("CONTRAATAQUE","ataque hierba lazo",+80);
     static Habilidad habilidadTres =new Habilidad("INFORTUNIO","ataque bola sombra",+79);
     static Habilidad habilidadCuatro= new Habilidad("CONFUSION","ataque onda mental",+100);
     static Habilidad habilidadCinco =new Habilidad("GIRO FUEGO","ataque bola sombra",+88);
+    static Habilidad habilidadSeis=new Habilidad("COLMILLO ÍGNEO","ataque Sofoco",+89);
+
+    public static void main(String[] args) {
+        habilidades.add(habilidadUno);
+        habilidades.add(habilidadDos);
+        habilidades.add(habilidadTres);
+        habilidades.add(habilidadCuatro);
+        habilidades.add(habilidadCinco);
+        habilidades.add(habilidadSeis);
+    }
 
     @Override
     public void tiempoJugado(long tiempoInicial, long tiempoFInal) {
