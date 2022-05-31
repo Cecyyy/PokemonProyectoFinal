b50922f9b4b82f5e655a76b6dea244e42cb37473
Cecyyy <cecyparamo94@gmail.com>
2022-05-30 20:06:57 -0500

    casos con lo nombres aleatorios completo, son siete tipos

--- src/Main.java
+++ src/Main.java
@@ -46,11 +46,111 @@ public class Main implements UtilInterface {
 
     @Override
     public String[] debilYFuerteAleatorio(String tipo) {
-        return new String[0];
+        //[0]----> DEBIL
+
+        // [1]---->FUERTE
+        //VERIFICAR SI LA LOGICA DEDEBIL Y FUERTE
+        // CUMPLE  CON CADA TIPO
+        //FALTA POR VER CUNT TIPOS VAMOS HACER
+        String debilYFuerte []= new String[2];
+        switch (tipo){
+            case ("HADA"):
+                debilYFuerte[0]="DRAGON";//DEBIL CONTRA
+                debilYFuerte[1]="FUEGO";//FUERTE CONTRA
+                break;
+            case ("DRAGON"):
+                debilYFuerte[0]="PSIQUICO";//DEBIL CONTRA
+                debilYFuerte[1]="HADA";//FUERTE CONTRA
+                break;
+
+            case ("FUEGO"):
+                debilYFuerte[0]="HADA";//DEBIL CONTRA
+                debilYFuerte[1]="PLANTA";//FUERTE CONTRA
+                break;
+            case ("PSIQUICO"):
+                debilYFuerte[0]="SINIESTRO";//DEBIL CONTRA
+                debilYFuerte[1]="DRAGON";//FUERTE CONTRA
+                break;
+            case ("PLANTA"):
+                debilYFuerte[0]="FUEGO";//DEBIL CONTRA
+                debilYFuerte[1]="AGUA";//FUERTE CONTRA
+                break;
+            case ("SINIESTRO"):
+                debilYFuerte[0]="HADA";//DEBIL CONTRA
+                debilYFuerte[1]="PSIQUICO";//FUERTE CONTRA
+            case ("AGUA"):
+                debilYFuerte[0]="DRAGON";//DEBIL CONTRA
+                debilYFuerte[1]="HADA";//FUERTE CONTRA
+
+
+
+        }
+        return debilYFuerte;
     }
 
     @Override
     public String[] nombresaleatorios(String tipo) {
-        return new String[0];
+        String namjoon[]=new String[6];
+        //siete tipos con seis pokemones diferentes c/u
+        switch (tipo){
+            case ("HADA"):
+                namjoon [0]="Jigglypuff";
+                namjoon[1]="Sylveon";
+                namjoon[2]="Togepi";
+                namjoon[3]="Clefairy";
+                namjoon[4]="Xernas";
+                namjoon[5]="Mew";
+                break;
+            case ("DRAGON"):
+                namjoon [0]="Altaria";
+                namjoon[1]="Goodra";
+                namjoon[2]="Goomy";
+                namjoon[3]="Axew";
+                namjoon[4]="Dragonair";
+                namjoon[5]="Dratini";
+                break;
+            case ("FUEGO"):
+                namjoon [0]="Ponyta";
+                namjoon[1]="Arcanine";
+                namjoon[2]="Charizard";
+                namjoon[3]="Flareon";
+                namjoon[4]="Vulpix";
+                namjoon[5]="Ninetales";
+                break;
+            case ("PSIQUICO"):
+                namjoon [0]="Lunatone";
+                namjoon[1]="Mewtwo";
+                namjoon[2]="Gothorita";
+                namjoon[3]="Necrozma";
+                namjoon[4]="Musharna";
+                namjoon[5]="Espeon";
+                break;
+            case ("PLANTA"):
+                namjoon [0]="Leafeon";
+                namjoon[1]="Celebi";
+                namjoon[2]="Chikorita";
+                namjoon[3]="Tangela";
+                namjoon[4]="Sprigatito";
+                namjoon[5]="Bayleef";
+                break;
+            case ("SINIESTRO"):
+                namjoon [0]="Umbreon";
+                namjoon[1]="Absol";
+                namjoon[2]="Meowth";
+                namjoon[3]="Mightyena";
+                namjoon[4]="Darkrai";
+                namjoon[5]="Zorua";
+                break;
+            case ("AGUA"):
+                namjoon [0]="Psyduck";
+                namjoon[1]="Piplup";
+                namjoon[2]="Milotic";
+                namjoon[3]="Suicune";
+                namjoon[4]="Vaporeon";
+                namjoon[5]="Phione";
+                break;
+
+
+        }
     }
 }
